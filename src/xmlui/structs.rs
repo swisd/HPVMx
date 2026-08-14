3af2d57d198c6d7748abd5677199d4c8e527c56a
nitrogen-12 <119827347+swisd@users.noreply.github.com>
2026-08-13 22:53:20 -0400

    Update structs.rs

--- src/xmlui/structs.rs
+++ src/xmlui/structs.rs
@@ -4,7 +4,14 @@ use hashbrown::HashMap;
 
 pub type NodeProp = (String, String);
 
+enum NodeDataType {
+    Text(String),
+    Number(i64),
+    Boolean(bool),
+}
+
 pub struct Node {
     pub children: Option<Vec<Node>>,
     pub properties: HashMap<String, String>,
+    pub data: NodeDataType,
 }
