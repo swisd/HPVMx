f9c843ecbaafb34d99737d957bfb44a31c8f7fee
swisd <glordi053@gmail.com>
2026-08-20 08:25:53 -0400

    remove uefi_test_loop binary

--- src/bin/uefi_test_loop.rs
+++ src/bin/uefi_test_loop.rs
@@ -1,18 +1,18 @@
-#![no_std]
-#![no_main]
-
-use core::hint::spin_loop;
-use uefi::prelude::*;
-use uefi::println;
-
-#[entry]
-fn main() -> Status {
-    uefi::helpers::init().unwrap();
-
-    println!("HPVMx UEFI test app started.");
-    println!("Entering infinite loop for VM run validation...");
-
-    loop {
-        spin_loop();
-    }
-}
+// #![no_std]
+// #![no_main]
+//
+// use core::hint::spin_loop;
+// use uefi::prelude::*;
+// use uefi::println;
+//
+// #[entry]
+// fn main() -> Status {
+//     uefi::helpers::init().unwrap();
+//
+//     println!("HPVMx UEFI test app started.");
+//     println!("Entering infinite loop for VM run validation...");
+//
+//     loop {
+//         spin_loop();
+//     }
+// }
